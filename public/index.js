99eeda8d4bbf2c67f322d8259f78c2c056b86c6f
Cristian Nobili <c.nobili@gmail.com>
2024-01-22 12:09:36 +0100

    sistemato il todo

--- public/index.js
+++ public/index.js
@@ -1,10 +1,71 @@
-const template = `<li %ID" class="list-group-item">
+const template = `<li class="list-group-item %BG">
    <span>%TODO</span>
-   <button id="delete_ID" type="button" class="btn btn-danger float-end">X</button>
-   <button id="success_ID" type="button" class="btn btn-success float-end">V</button>
+   <button id="delete_ID" type="button" class="todo btn btn-danger float-end">X</button>
+   <button id="success_ID" type="button" class="todo btn btn-success float-end">V</button>
    </li>`;
 
-const todos = [];
+let todos = [];
 const todoInput = document.getElementById("todoInput");
 const insertButton = document.getElementById("insertButton");
+const listUL = document.getElementById("listUL");
+
+const send = (todo) => {
+   return new Promise((resolve, reject) => {
+      fetch("/todo/add", {
+         method: 'POST',
+         header: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify(todo)
+      }).then((response) => response.json)
+      .then((json) => {
+         resolve(json);
+      })
+   })
+}
+
+insertButton.onclick = () => {
+   const todo = {      
+      id: "" + new Date().getTime(),
+      name: todoInput.value,
+      completed: false
+   }
+   todos.push(todo);
+   render();
+   //send({todo: todo});
+}
+
+const render = () => {
+   listUL.innerHTML  = todos.map((todo) => {
+      let row = template.replace("delete_ID", "delete_"+todo.id);
+      row = row.replace("success_ID", "success_"+todo.id);
+      row = row.replace("%TODO", todo.name);      
+      row = row.replace("%BG", todo.completed ? "bg-success" : "");
+
+      return row;
+   }).join("\n");   
+   const buttonList = document.querySelectorAll(".todo");
+   buttonList.forEach((button) => {
+         button.onclick = () => {
+            if (button.id.indexOf("delete_") != -1) {
+               const id = button.id.replace("delete_", "");
+               todos = todos.filter((todo) => todo.id !== id);
+            } 
+            if (button.id.indexOf("success_") != -1) {
+               const id = button.id.replace("success_", "");
+               todos = todos.map((todo) => {
+                  if (todo.id === id) {
+                     todo.completed = !todo.completed;
+                  }
+                  return todo;
+               })
+            }
+            render();
+         }
+   })
+}
+
+render();
+
+
 
